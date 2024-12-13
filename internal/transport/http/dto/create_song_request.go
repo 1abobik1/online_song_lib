a3b39e7f60ab33880ccce8809a0ed4cee7f5df66
1abobik1 <dima15042004@mail.ru>
2024-12-13 10:02:15 +0300

    fix

--- internal/transport/http/dto/create_song_request.go
+++ internal/transport/http/dto/create_song_request.go
@@ -1,17 +1,6 @@
 package dto
 
-import "time"
-
 type CreateSongRequest struct {
 	Group string `json:"group"`
 	Song  string `json:"song"`
-}
-
-type SongResponse struct {
-	ID          int       `json:"id"`
-	GroupName   string    `json:"group_name"`
-	SongName    string    `json:"song_name"`
-	ReleaseDate time.Time `json:"release_date"`
-	Text        string    `json:"text"`
-	Link        string    `json:"link"`
 }
